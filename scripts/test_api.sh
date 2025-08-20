6c31c08b2d7423ee6aa4f0d9e35fb663e4b3c47f
RuanWernerKrugerInterversialSystem <ai_your_enterprise@interversialsystemofsystems.com>
2025-08-20 13:42:48 +0200

    Update test_api.sh

--- scripts/test_api.sh
+++ scripts/test_api.sh
@@ -4,4 +4,5 @@
 curl -X POST http://localhost:3000/api/gtt/issue -H "Authorization: Bearer $JWT_TOKEN" -d '{"id":"GTT002","owner":"UN_Treasury","value":2000}'
 curl -X GET http://localhost:3000/api/gtt/query/GTT002 -H "Authorization: Bearer $JWT_TOKEN"
 curl -X POST http://localhost:3000/api/compliance/verify -H "Authorization: Bearer $JWT_TOKEN" -d '{"transactionId":"TX001","userId":"USER001"}'
+curl -X GET http://localhost:3000/api/ledger/transactions
 echo "API tests completed"
